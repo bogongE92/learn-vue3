944232f323c2fab756d4ac8fb3de9604b93c3da0
Lena <112360499+bogongE92@users.noreply.github.com>
2024-05-07 19:32:31 +0900

    Template Refs 강의 추가

---
+++ src/components/TemplateRefChild.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		{{ $parent }}
+		<ul>
+			<li v-for="bread in $parent.breads" :key="bread">{{ bread }}</li>
+		</ul>
+		<!-- 자식 컴포넌트에서 상위 컴포넌트 참조하기 위해서는 $parent 내장객체를 사용할 수 있습니다. -->
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+
+export default {
+	setup() {
+		const msg = ref('Hello~!');
+		const sayHello = () => {
+			alert(msg.value); 
+		};
+		return {
+			msg,
+			sayHello
+		};
+	},
+};
+</script>
+<style lang="scss" scoped></style>
---
+++ src/components/TheView_TemplateRefs.vue
@@ -0,0 +1,18 @@
+<template>
+	<main>
+        <TemplateRef></TemplateRef> 
+    </main>
+</template>
+
+<script>
+import TemplateRef from "@/components/TemplateRef.vue"
+export default {
+	components : {
+        TemplateRef
+    },
+    setup() {
+		return {};
+	},
+};
+</script>
+<style lang="scss" scoped></style>
