e4825d854fb3d3090be527026424efccc5d8d5d1
Lena <112360499+bogongE92@users.noreply.github.com>
2024-05-09 12:11:08 +0900

    script setup 강의 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,15 +2,35 @@
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
-  root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
-  // axios await 오류 잡아주기
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  }
+	root: true,
+	extends: [
+		'plugin:vue/vue3-essential',
+		'eslint:recommended',
+		'@vue/eslint-config-prettier/skip-formatting'
+	],
+	// axios await 오류 잡아주기
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module'
+	},
+	env: {
+		'vue/setup-compiler-macros': true,
+	},
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-unused-vars': 'off',
+		/* 'prettier/prettier': [
+			'error',
+			{
+				singleQuote: true, // single 쿼테이션 사용 여부
+				semi: true, // 세미콜론 사용 여부
+				useTabs: true, // 탭 사용 여부
+				tabWidth: 4,
+				trailingComma: 'all', // 여러 줄을 사용할 때, 후행 콤마 사용 방식
+				printWidth: 80, //  줄 바꿈 할 폭 길이
+				bracketSpacing: false, // 객체 리터럴에서 괄호에 공백 삽입 여부 
+				arrowParens: 'avoid', // 화살표 함수 괄호 사용 방식
+			},
+		], */
+	},
 }
---
+++ src/components/setup/MyButton_ScriptSetup.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="p-3 bg-danger">
+        <button class="btn btn-primary" type="button" v-bind="$attrs">
+            My Button {{ msg }}
+        </button>
+        <!-- {{$attrs}} -->
+    </div>
+</template>
+<!--  
+    inheritAttrs 같은 경우에는 non-prop 속성을 루트에 상속할 것인지 그런 속성인데 
+    <script setup>에서는 지원을 안하기 때문에
+    <script setup>과 <script> 일반 스크립트를 같이 사용한다.
+    이렇게 옵션을 사용해야 하는 경우에 함께 사용할 수 있다.
+
+-->
+<script>
+export default {
+    // 상속 비활성화
+    inheritAttrs:false, 
+}
+</script>
+
+<script setup>
+import { ref } from 'vue';
+
+//반응형 선언
+const msg = ref('message');
+</script>
+
+<style lang="scss" scoped></style>
